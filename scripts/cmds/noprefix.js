bf1495fc5c4b2ff9412beeca6cd518ba063f290c
ewrnahid <nahidul2.000001@gmail.com>
2026-03-10 00:14:51 +0600

    Update noprefix.js

--- scripts/cmds/noprefix.js
+++ scripts/cmds/noprefix.js
@@ -35,8 +35,8 @@ module.exports = {
 				reply: "আমি আপনার জন্য অপেক্ষা করছিলাম! 😄"
 			},
 			{
-				keyullash: ["ullash bai", "Ullash"],
-				reply: "উল্লাস বস ব্যস্ত আছে কি বলবি আমাকে বল ❤️"
+				keyullash: ["naim bai", "Nahidul"],
+				reply: "নাইম বস ব্যস্ত আছে কি বলবি আমাকে বল ❤️"
 			}
 		];
 
