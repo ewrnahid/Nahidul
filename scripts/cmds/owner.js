9e2da295c2aa9cc3d4f34d9cfee16bf379be606e
ewrnahid <nahidul2.000001@gmail.com>
2026-03-10 11:06:11 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -24,7 +24,7 @@ config: {
       nick: 'NAIM'
     };
 
-    const bold = 'https://files.catbox.moe/o9e2cg.mp4; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
+    const bold = 'https://files.catbox.moe/o9e2cg.mp4';  // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
 
     const tmpFolderPath = path.join(__dirname, 'tmp');
 
