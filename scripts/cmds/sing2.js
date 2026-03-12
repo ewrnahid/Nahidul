4f9e69b8497a1316a5cc770d4bf275b07dd8ca09
ewrnahid <nahidul2.000001@gmail.com>
2026-03-13 00:44:39 +0600

    Rename command from 'sing' to 'sing2'

--- scripts/cmds/sing2.js
+++ scripts/cmds/sing2.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 module.exports = {
   config: {
-    name: "sing",
+    name: "sing2",
     aliases: ["song", "music", "play"],
     version: "2.0",
     author: "Naim",
