35fb463e60a01ac0c4fccf6773afecf54eaaf737
ewrnahid <nahidul2.000001@gmail.com>
2026-03-24 13:08:14 +0600

    Update love3.js

--- scripts/cmds/love3.js
+++ scripts/cmds/love3.js
@@ -12,7 +12,7 @@ module.exports.config = {
   cooldowns: 5
 };
 
-module.exports.run = async function({ api, event }) {
+module.exports.onStart = async function({ api, event }) {
   const mentions = Object.keys(event.mentions);
 
   if (mentions.length === 0) {
